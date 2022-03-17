32d09fb6be7f66e64fabd3eae2d5e7d0f3c2a4b0
Nairod <do.gaspart@gmail.com>
2022-03-17 10:30:08 +0100

    Ajout du test de la valeur de numero de semaine

--- ClientDateCalendrier.java
+++ ClientDateCalendrier.java
@@ -2,5 +2,6 @@ public class ClientDateCalendrier{
     public static void main(String[]args){
         DateCalendrier cal1 = new DateCalendrier();
         System.out.println(cal1);
+        System.out.println(cal1.getNoSem());
     }
 }
